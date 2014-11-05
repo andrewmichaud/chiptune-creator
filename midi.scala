99be8cc4ce1a71db5ce2eb44c8e575cb215b8b0b
Andrew Michaud <andrewjmichaud@gmail.com>
2014-11-05 14:52:20 -0800

    create simple midi creation program in scala. Will clean it up to make music writing easier.

---
+++ midi.scala
@@ -0,0 +1,96 @@
+/**
+ *
+ * Generating MIDI with Scala
+ *
+ * author Andrew Michaud
+ * converted from code by Karl Brown- automatic-pilot.com/midifile.html
+ * date 11/05/14
+ */
+
+import java.io._
+import java.util._
+import javax.sound.midi._
+
+object MIDIGen {
+  def main(args: Array[String]) {
+    println("midifile begin ")
+
+    try {
+
+      // Create MIDI sequence with 24 ticks/beat
+      var s = new Sequence(javax.sound.midi.Sequence.PPQ, 24)
+
+      // Obtain MIDI track of sequence.
+      var t = s.createTrack()
+
+      // General MIDI stuff? -- turn on General MIDI sound set
+      // NOTE I hear java doesn't like signed bytes, that might be an issue.
+      var b:Array[Byte] = Array(0xF0.toByte, 0x7E, 0x7F, 0x09, 0x01, 0xF7.toByte)
+      var sm = new SysexMessage()
+      sm.setMessage(b, 6)
+      var me = new MidiEvent(sm, 0)
+      t.add(me)
+
+      // Set tempo (meta event)
+      var mt = new MetaMessage()
+      var bt: Array[Byte] = Array(0x02, 0x00, 0x00)
+      mt.setMessage(0x51, bt, 3)
+      me = new MidiEvent(mt, 0)
+      t.add(me)
+
+      // Set track name (meta event)
+      mt = new MetaMessage()
+      var TrackName = new String("midifile track")
+      mt.setMessage(0x03, TrackName.getBytes(), TrackName.length())
+      me = new MidiEvent(mt, 0)
+      t.add(me)
+
+      // Set omni on
+      var mm = new ShortMessage()
+      mm.setMessage(0xB0, 0x7D, 0x00)
+      me = new MidiEvent(mm, 0)
+      t.add(me)
+
+      // Set poly on
+      mm = new ShortMessage()
+      mm.setMessage(0xB0, 0x7F, 0x00)
+      me = new MidiEvent(mm, 0)
+      t.add(me)
+
+      // Set instrument to Piano
+      // TODO wrap instruments for cleaner syntax
+      mm = new ShortMessage()
+      mm.setMessage(0xC0, 0x00, 0x00)
+      me = new MidiEvent(mm, 0)
+      t.add(me)
+
+      // Note on - middle C
+      mm = new ShortMessage()
+      mm.setMessage(0x90,0x3C,0x60)
+		  me = new MidiEvent(mm, 1)
+		  t.add(me)
+
+      // Note off - middle C - 120 ticks later
+      mm = new ShortMessage()
+		  mm.setMessage(0x80,0x3C,0x40)
+		  me = new MidiEvent(mm, 121)
+		  t.add(me)
+
+      // Set end of track (meta event) 19 ticks later
+      mt = new MetaMessage()
+      var bet: Array[Byte] = Array() // empty array
+		  mt.setMessage(0x2F, bet, 0)
+		  me = new MidiEvent(mt, 140)
+		  t.add(me)
+
+      // Write MIDI to MIDI file
+      var f = new File("testmidi.mid")
+      MidiSystem.write(s, 1, f)
+
+    } catch {
+      case e: Exception => println("Exception: " + e.toString())
+    }
+    println("midifile end ")
+  }
+}
+
