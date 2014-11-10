8936213376459fd5114a76b19a345eb14738e5ed
Andrew Michaud <andrewjmichaud@gmail.com>
2014-11-10 14:32:17 -0800

    redid code using a much simpler midi example. MIDI is now interpreted by Scala/Java correctly.  I can play notes really easily one after the other with any duration. Next, I'll try to write chords and write a simple song.

--- midi.scala
+++ midi.scala
@@ -3,7 +3,6 @@
  * Generating MIDI with Scala
  *
  * author Andrew Michaud
- * converted from code by Karl Brown- automatic-pilot.com/midifile.html
  * date 11/05/14
  */
 
@@ -15,67 +14,16 @@ import sys.process._
 
 object MIDIGen {
   // Data members
-  // Create MIDI sequence with 24 ticks/beat
-  var s = new Sequence(javax.sound.midi.Sequence.PPQ, 24)
-
-  // Obtain MIDI track of sequence.
-  var t = s.createTrack()
-
-  // Abstraction functions.  Might deserve their own class or different organization at some
-  // point.
-  // NOTE tempo argument ignored until I can reverse-engineer setting MIDI tempo.
-  def initMIDI(trackname: String, tempo: Int) {
-      // General MIDI stuff? -- turn on General MIDI sound set
-      // NOTE I hear java doesn't like signed bytes, that might be an issue.
-      var b:Array[Byte] = Array(0xF0.toByte, 0x7E, 0x7F, 0x09, 0x01, 0xF7.toByte)
-      var sm = new SysexMessage()
-      sm.setMessage(b, 6)
-      var me = new MidiEvent(sm, 0)
-      t.add(me)
-
-
-      // Set tempo (meta event)
-      var mt = new MetaMessage()
-      var bt: Array[Byte] = Array(0x02, 0x00, 0x00)
-      mt.setMessage(0x51, bt, 3)
-      me = new MidiEvent(mt, 0)
-      t.add(me)
-
-      // Set track name (meta event)
-      mt = new MetaMessage()
-      var TrackName = new String(trackname)
-      mt.setMessage(0x03, TrackName.getBytes(), TrackName.length())
-      me = new MidiEvent(mt, 0)
-      t.add(me)
-
-      // Set omni on
-      var mm = new ShortMessage()
-      mm.setMessage(0xB0, 0x7D, 0x00)
-      me = new MidiEvent(mm, 0)
-      t.add(me)
-
-      // Set poly on
-      mm = new ShortMessage()
-      mm.setMessage(0xB0, 0x7F, 0x00)
-      me = new MidiEvent(mm, 0)
-      t.add(me)
-  }
+  var synth = MidiSystem.getSynthesizer()
+  var channels = synth.getChannels()
 
   // Play a given note with given velocity and on the given channel.
-  def playNote(note: Note = C3, velocity: Int = 60, channel: Int = 1, duration: Int = 60) {
+  def playNote(note: Note = C3, velocity: Int = 60, channel: Int = 0, duration: Int = 200) {
     // TODO check range on inputs.
 
-    // Note on
-    var mm = new ShortMessage()
-    mm.setMessage(ShortMessage.NOTE_ON, channel, note.name, velocity)
-    var me = new MidiEvent(mm, 1)
-    t.add(me)
-
-    // Note off
-    mm = new ShortMessage()
-    mm.setMessage(ShortMessage.NOTE_OFF, channel, note.name, velocity)
-    me = new MidiEvent(mm, duration + 1)
-    t.add(me)
+    channels(channel).noteOn(note.name, velocity)
+    Thread.sleep(duration)
+    channels(channel).noteOff(note.name)
 
   }
 
@@ -135,41 +83,18 @@ object MIDIGen {
     println("midifile begin ")
 
     try {
-      initMIDI("test midifile", 5)
+      // Open synthesizer and get channels.
+      synth.open()
 
-      // Set instrument to Piano
-      // TODO wrap instruments for cleaner syntax
-      // NOTE find out what the hell is happening here so I can make some constants and
-      // wrap this function up.
-      var mm = new ShortMessage()
-      mm.setMessage(0xC0, 0x00, 0x00)
-      var me = new MidiEvent(mm, 0)
-      t.add(me)
-
-      playNote()
+      // Play notes.
+      playNote(note = D3)
       playNote(note = C3)
-      playNote()
-
-
-      // Set end of track (meta event) 19 ticks later
-      var mt = new MetaMessage()
-      var bet: Array[Byte] = Array() // empty array
-		  mt.setMessage(0x2F, bet, 0)
-		  me = new MidiEvent(mt, 140)
-		  t.add(me)
-
-      // Write MIDI to MIDI file
-      var f = new File("testmidi.mid")
-      MidiSystem.write(s, 1, f)
+      playNote(note = B2)
+      playNote(note = A2)
 
     } catch {
-      case e: Exception => println("Exception: " + e.toString())
+      case e: Exception => println("Exception: " + e.printStackTrace())
     }
-    println("midifile end ")
-
-    // Create mp3 from midi using external app.
-    // TODO make this less dependent on an external app.
-    val convertresult = "./midtomp3 testmidi"
   }
 }
 
