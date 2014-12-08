cb1c5d6dda61a4fc9c0273883c4d5c7bca6e00e2
Andrew Michaud <andrewjmichaud@gmail.com>
2014-12-07 18:56:19 -0800

    permissions changed I guess?

--- src/main/scala/chiptune.scala
+++ src/main/scala/chiptune.scala
@@ -17,9 +17,38 @@ object ChiptuneCreator extends EvalLoop with App {
   override def prompt = ""
 
   loop { line =>
-    ChipParser(line) match {
-      case ChipParser.Success(t,_) => println(eval(t))
-      case e: ChipParser.NoSuccess => println(e)
+
+    // Switch between interpreting and parsing modes.
+    line match {
+      case "Interpreter" => {
+        ChipParser(line) match {
+          case ChipParser.Success(t,_) => println(eval(t))
+          case e: ChipParser.NoSuccess => println(e)
+        }
+      }
+
+      case "Compiler" => {
+        println("Please specify file name.  Assumes project root directory.")
+
+        try {
+          val lines = io.Source.fromFile(line).getLines().toList
+          // Parse and run each line.
+
+          for (line <- lines) {
+            ChipParser(line) match {
+              case ChipParser.Success(t,_) => println(eval(t))
+              case e: ChipParser.NoSuccess => println(e)
+            }
+          }
+        } catch {
+          case e: Exception => println(e)
+        }
+      }
+
+      case _ => {
+        println("Please specify either Interpreter or Compiler.")
+      }
     }
+
   }
 }
