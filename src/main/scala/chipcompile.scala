f5982a9a68ab93962f1839a14ec6b27604b5c4ba
Andrew Michaud <andrewjmichaud@gmail.com>
2014-12-07 18:56:52 -0800

    add compiler

---
+++ src/main/scala/chipcompile.scala
@@ -0,0 +1,40 @@
+
+package com.andrewmichaud.chiptune
+
+/**
+ * Andrew Michaud
+ *
+ * Chiptune compiler.  Separated from interpreter for simplicity.
+ *
+ */
+
+import scala.tools.nsc.EvalLoop
+import com.andrewmichaud.chiptune.parser.ChipParser
+import com.andrewmichaud.chiptune.semantics.eval
+
+object ChiptuneCompiler extends EvalLoop with App {
+  override def prompt = ""
+
+  loop { line =>
+
+    // Attempt to load file.
+
+    try {
+      val lines = io.Source.fromFile(line).getLines().toList
+
+      // Parse and apply each line.
+      for (line <- lines) {
+        ChipParser(line) match {
+          case ChipParser.Success(t, _) => eval(t)
+          case e: ChipParser.NoSuccess  => println(e)
+        }
+      }
+    } catch {
+      case fnf: java.io.FileNotFoundException => {
+        println("File not found!")
+        println("Specify a valid file found in the project root directory.")
+      }
+      case e: Exception => println(e)
+    }
+  }
+}
---
+++ src/main/scala/chipinterpret.scala
@@ -0,0 +1,25 @@
+package com.andrewmichaud.chiptune
+
+/**
+ * Andrew Michaud
+ *
+ * 11/24/14
+ *
+ * Main eval loop, where all the magic happens.
+ *
+ */
+
+import scala.tools.nsc.EvalLoop
+import com.andrewmichaud.chiptune.parser.ChipParser
+import com.andrewmichaud.chiptune.semantics.eval
+
+object ChiptuneInterpreter extends EvalLoop with App {
+  override def prompt = ""
+
+  loop { line =>
+    ChipParser(line) match {
+      case ChipParser.Success(t,_) => println(eval(t))
+      case e: ChipParser.NoSuccess => println(e)
+    }
+  }
+}
